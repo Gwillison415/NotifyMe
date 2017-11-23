7d9c96b885017438e26c44d9cbbe697e1e322853
Grant Willison <emailgdw@gmail.com>
2017-11-23 12:59:09 -0800

    Improve params

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ import NavToolBar from './components/navbar';
 // const P = Particles.default;
 const P = Particles;
 
-
 class App extends Component {
   // the would be normal method to accept incoming data upon mounting of main component
   // I'd probably use an render-if logic if it was a multi page app
@@ -29,126 +28,21 @@ class App extends Component {
   render() {
     return (<Container>
       <P params={{
-        //  particlesConfig
-        "particles": {
-          "number": {
-            "value": 80,
-            "density": {
-              "enable": true,
-              "value_area": 800
-            }
-          },
-          "color": {
-            "value": "#ffffff"
-          },
-          "shape": {
-            "type": "circle",
-            "stroke": {
-              "width": 0,
-              "color": "#000000"
-            },
-            "polygon": {
-              "nb_sides": 6
-            },
-            "image": {
-              "src": "img/github.svg",
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 0.5,
-            "random": false,
-            "anim": {
-              "enable": false,
-              "speed": 1,
-              "opacity_min": 0.1,
-              "sync": false
-            }
-          },
-          "size": {
-            "value": 3,
-            "random": true,
-            "anim": {
-              "enable": false,
-              "speed": 40,
-              "size_min": 0.1,
-              "sync": false
-            }
-          },
-          "line_linked": {
-            "enable": true,
-            "distance": 150,
-            "color": "#ffffff",
-            "opacity": 0.4,
-            "width": 1
-          },
-          "move": {
-            "enable": true,
-            "speed": 3.1565905665290903,
-            "direction": "none",
-            "random": true,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-              "enable": true,
-              "rotateX": 3235.5053306923182,
-              "rotateY": 2683.101981549727
-            }
-          }
-        },
-        "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-            "onhover": {
-              "enable": true,
-              "mode": "grab"
-            },
-            "onclick": {
-              "enable": true,
-              "mode": "bubble"
-            },
-            "resize": true
-          },
-          "modes": {
-            "grab": {
-              "distance": 400,
-              "line_linked": {
-                "opacity": 1
-              }
-            },
-            "bubble": {
-              "distance": 400,
-              "size": 40,
-              "duration": 2,
-              "opacity": 8,
-              "speed": 3
-            },
-            "repulse": {
-              "distance": 200,
-              "duration": 0.4
-            },
-            "push": {
-              "particles_nb": 4
-            },
-            "remove": {
-              "particles_nb": 2
-            }
-          }
-        }
-        //  particles: {
-        //    number: {
-        //      value: 150
-        //    },
-        //    line_linked: {
-        //      shadow: {
-        //        enable: false,
-        //        color: "#3CA9D1",
-        //        blur: 5
-        //      }
-        //    }
-        //  }
+        // particles: particlesConfig.particles,
+        // interactivity : particlesConfig.interactivity,
+
+         particles: {
+           number: {
+             value: 150
+           },
+           line_linked: {
+             shadow: {
+               enable: false,
+               color: "#3CA9D1",
+               blur: 5
+             }
+           }
+         }
        }} style={{
          position: "fixed",
          "z-index": 4,
